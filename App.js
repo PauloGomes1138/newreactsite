a6b3f66c0a2c4aba1347070abbffe0bc0f9c3b36
Paulo Gomes <139072768+PauloGomes1138@users.noreply.github.com>
2023-12-10 18:25:19 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import navbar from "./navbar";
+
+function app(){
+return <navbar/> ();
+
+}
+
+export default app
---
+++ navbar.js
@@ -0,0 +1,28 @@
+export default function navbar(){
+    return <><nav className="nav">
+        // eslint-disable-next-line jsx-a11y/anchor-has-content
+        <a href="/" className="site-title"/>
+        SOFTEXLABS
+        <ul>
+            <li>
+                <a href="/softexlabs">SOFTEXLABS</a>
+                
+            </li>
+                <li>
+                <a href="/Games">GAMES</a>
+                </li>
+                <li>
+                <a href="/iot">IOT</a>
+                </li>
+                <li>
+                <a href="/weatherstation">WEATHER STATION</a>
+                </li>
+                <li>
+                <a href="/eco">ECO</a>
+                </li>
+                <li>
+                <a href="/robots">ROBOTS</a>
+                </li>
+        </ul>
+        </nav></>
+        }
